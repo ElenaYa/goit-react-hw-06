7c8ae5ae01a9c7e53cd1581c90a042e3ff74477e
ElenaYa <elena.yatsynyuk@gmail.com>
2024-04-04 23:56:34 +0300

    react-redux

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
-function App() {
+import ContactForm from "./components/ContactForm/ContactForm";
+import SearchBox from "./components/SearchBox/SearchBox";
+import ContactList from "./components/ContactList/ContactList";
+
+export default function App() {
   return (
-    <>
-      
-    </>
-  )
+    <div className="containerApp">
+      <h1 className="titleApp">Phonebook</h1>
+      <ContactForm />
+      <SearchBox />
+      <ContactList />
+    </div>
+  );
 }
 
-export default App
